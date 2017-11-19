f37a69b520ec68ae39def8b6a6866b7db14b632e
Bozhi Liu <bozhiliu@email.arizona.edu>
2017-11-19 14:22:07 -0700

    remove redundant

--- iot-device/sunsmart_full.ino
+++ iot-device/sunsmart_full.ino
@@ -49,72 +49,13 @@ void loop() {
    
 
     char c = GPS.read();
-    // if you want to debug, this is a good time to do it!
-    if (GPSECHO)
-        if (c) Serial.print(c);
-    // if a sentence is received, we can check the checksum, parse it...
-    if (GPS.newNMEAreceived()) {
-        // a tricky thing here is if we print the NMEA sentence, or data
-        // we end up not listening and catching other sentences!
-        // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-        Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-        if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-          return; // we can fail to parse a sentence in which case we should just wait for another
-      }
-    // if millis() or timer wraps around, we'll just reset it
-    if (timer > millis()) timer = millis();
          
     // approximately every 2 seconds or so, print out the current stats
     if (millis() - timer > 2000) {
-        timer = millis(); // reset the timer
-        Serial.print("\nTime: ");
-        Serial.print(GPS.hour, DEC); Serial.print(':');
-        Serial.print(GPS.minute, DEC); Serial.print(':');
-        Serial.print(GPS.seconds, DEC); Serial.print('.');
-        Serial.println(GPS.milliseconds);
-        Serial.print("Date: ");
-        Serial.print(GPS.day, DEC); Serial.print('/');
-        Serial.print(GPS.month, DEC); Serial.print("/20");
-        Serial.println(GPS.year, DEC);
-        Serial.print("Fix: "); Serial.print((int)GPS.fix);
-        Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-        if (GPS.fix) {
-            Serial.print("Location: ");
-            Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-            Serial.print(", ");
-            Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-            Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-            Serial.print("Angle: "); Serial.println(GPS.angle);
-            Serial.print("Altitude: "); Serial.println(GPS.altitude);
-            Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-        }
-    
-    
-        if (digitalRead(button) == 0) { // pulldown resistor, 0: Pressed
-            uvString = String(uv.readUV());
-            // Construct json string
-            
-            
-          //  String data = String("{ \"longitude\": -110.950258, \"latitude\": 32.227698, \"uv\": 222 }");
-    
-            String data1 = String("{ \"longitude\": ");
-            String data2 = String(", \"latitude\": ");
-            String data3 = String(", \"uv\": 222 }");
-            String latitude = String(GPS.latitude);
-            latitudeString = String(GPS.latitude);
-            longitudeString = String(GPS.longitude);
-            String longitude = String(GPS.longitude);
-            String data = data1 + longitude + data2 + latitude + data3;
-            // Log to serial console
-            
-            Serial.println(data);
-            Serial.println("button pressed!");
-            // Publish to webhook
-    //        Particle.publish("sunsmart", data, PRIVATE);
-        }
-    
+                        
     }
     response = sprintf(format, longitude, latitude, uv);
+    Serial.println(response);
     Particle.publish("sunsmart", response, PRIVATE)
     delay(500);
 }
