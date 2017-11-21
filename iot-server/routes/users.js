69587f198343ca172e646dc60bcc5e9a40cbfda3
Billy Tseng <tseng.billy@gmail.com>
2017-11-21 16:34:04 -0700

    style: justify code format.

--- iot-server/routes/users.js
+++ iot-server/routes/users.js
@@ -15,24 +15,24 @@ router.get('/', function(req, res, next) {
 
 /* Register a new user */
 router.post('/signup', function(req, res, next) {
-    // Create a hash for the submitted password
-    bcrypt.hash(req.body.password, null, null, function(err, hash) {
-    	// Prepare a new user
-    	var newUser = new User( {
-    	    email: req.body.email,
-    	    fullName: req.body.fullName,
-    	    passwordHash: hash // hashed password
-    	});
-    	newUser.save( function(err, user) {
-    	    if (err) { // this error could be a duplicate key error when the same email insertion tried
-              // console.log(err.errmsg); // show the db's error message. 
-              // For the safety reason, do not specify the detailed error message.
-    		      res.status(400).json( {success: false, message: "Bad Request" } );
-    	    } else {
-    		      res.status(201).json( {success: true, message: user.fullName + " has been created", redirect:"/signin.html" } );
-    	    }
-    	});
-    });
+  // Create a hash for the submitted password
+  bcrypt.hash(req.body.password, null, null, function(err, hash) {
+  	// Prepare a new user
+  	var newUser = new User( {
+  	    email: req.body.email,
+  	    fullName: req.body.fullName,
+  	    passwordHash: hash // hashed password
+  	});
+  	newUser.save( function(err, user) {
+      if (err) { // this error could be a duplicate key error when the same email insertion tried
+        // console.log(err.errmsg); // show the db's error message.
+        // For the safety reason, do not specify the detailed error message.
+        res.status(400).json( {success: false, message: "Bad Request" } );
+      } else {
+        res.status(201).json( {success: true, message: user.fullName + " has been created", redirect:"/signin.html" } );
+      }
+  	});
+  });
 });
 
 module.exports = router;
