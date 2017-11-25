9edd78dbb03a3f9fbd6049b1e143b5b328af8212
Billy Tseng <tseng.billy@gmail.com>
2017-11-25 10:49:56 -0700

    style: justify code format.

--- iot-server/routes/device.js
+++ iot-server/routes/device.js
@@ -54,68 +54,67 @@ router.post('/post', function(req, res, next) {
 
 // POST registers a new device given the device ID and user email
 router.post('/register', function(req, res, next) {
-    var responseJson = {
-        registered: false,
-        message : "",
-        apikey : "none"
-    };
-    var deviceExists = false;
+  var responseJson = {
+    registered: false,
+    message : "",
+    apikey : "none"
+  };
 
-    // Check if the X-Auth header is set
-    if (!req.headers["x-auth"]) {
-      responseJson.message = "Missing X-Auth header.";
-	    return res.status(401).json(responseJson);
-    }
-    // X-Auth should contain the token value
-    var token = req.headers["x-auth"];
+  // Check if the X-Auth header is set
+  if (!req.headers["x-auth"]) {
+    responseJson.message = "Missing X-Auth header.";
+    return res.status(401).json(responseJson);
+  }
+  // X-Auth should contain the token value
+  var token = req.headers["x-auth"];
 
-    // try decoding
-    try {
-      var decoded = jwt.decode(token, secret);
+  // try decoding
+  try {
+    var decoded = jwt.decode(token, secret);
 
-      // Ensure the request includes the deviceId parameter
-      for (var key in req.body) {
-          if (key != 'deviceId') {
-            responseJson.message = "Missing deviceId.";
-            res.status(400).json(responseJson);
-            return;
-          }
+    // Ensure the request includes the deviceId parameter
+    for (var key in req.body) {
+      if (key != 'deviceId') {
+        responseJson.message = "Missing deviceId.";
+        res.status(400).json(responseJson);
+        return;
       }
-
-      // See if device is already registered
-      Device.findOne({ deviceId: req.body.deviceId }, function(err, device) {
-          if (device !== null) {
-              responseJson.message = "Device ID " + req.body.deviceId + " already registered.";
-              res.status(400).json(responseJson);
-          } else {
-              // Get a new apikey
-             deviceApikey = getNewApikey();
-             // Create a new device with specified id, user email, and randomly generated apikey.
-              var newDevice = new Device({
-                  deviceId: req.body.deviceId,
-                  userEmail: decoded.email,
-                  apikey: deviceApikey
-              });
-
-              // Save device. If successful, return success. If not, return error message.
-              newDevice.save(function(err, newDevice) {
-                  if (err) {
-                      console.log("Error: " + err);
-                      responseJson.message = err;
-                      res.status(400).json(responseJson);
-                  } else {
-                      responseJson.registered = true;
-                      responseJson.apikey = deviceApikey;
-                      responseJson.message = "Device ID " + req.body.deviceId + " was registered.";
-                      res.status(201).json(responseJson);
-                  }
-              });
-          }
-      });
-    } catch (ex) {
-      responseJson.message = "Authentication error: " + ex.message;
-      res.status(401).json(responseJson);
     }
+
+    // See if device is already registered
+    Device.findOne({ deviceId: req.body.deviceId }, function(err, device) {
+      if (device !== null) {
+        responseJson.message = "Device ID " + req.body.deviceId + " already registered.";
+        res.status(400).json(responseJson);
+      } else {
+        // Get a new apikey
+        deviceApikey = getNewApikey();
+        // Create a new device with specified id, user email, and randomly generated apikey.
+        var newDevice = new Device({
+          deviceId: req.body.deviceId,
+          userEmail: decoded.email,
+          apikey: deviceApikey
+        });
+
+        // Save device. If successful, return success. If not, return error message.
+        newDevice.save(function(err, newDevice) {
+          if (err) {
+            // console.log("Error: " + err);
+            responseJson.message = err;
+            res.status(400).json(responseJson);
+          } else {
+            responseJson.registered = true;
+            responseJson.apikey = deviceApikey;
+            responseJson.message = "Device ID " + req.body.deviceId + " was registered.";
+            res.status(201).json(responseJson);
+          }
+        });
+      }
+    });
+  } catch (ex) {
+    responseJson.message = "Authentication error: " + ex.message;
+    res.status(401).json(responseJson);
+  }
 });
 
 // DELETE a device by deviceId
