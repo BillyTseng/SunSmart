807e849040d8811f0e0deebff55971d58a74f6cb
Billy Tseng <tseng.billy@gmail.com>
2017-12-05 16:19:01 -0700

    feat: mark all uv record on the map.

--- iot-server/public/javascripts/map.js
+++ iot-server/public/javascripts/map.js
@@ -1,15 +1,51 @@
-// Executes once the google map api is loaded, and then sets up the handler's and calls
-// getRecentPotholes() to display the recent potholes
-function initMap() {
+// GET the uv record
+function getUVRecord() {
+  $.ajax({
+      url: '/record/all',
+      type: 'GET',
+      responseType: 'json',
+      success: markUVRecord,
+      error: function(jqXHR, status, error) {
+        if (status === 401) {
+            window.localStorage.removeItem("token");
+            window.location = "signin.html";
+        } else {
+           alert(error);
+        }
+      }
+  });
+}
+
+function markUVRecord(data, status, xhr) {
   var latitude = 32.2319;
   var longitude = -110.9501;
 
-  // Create a map centered at the most recent pothole location
+  // Create a map centered at the location
   var uluru = {lat: latitude, lng: longitude};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: uluru
   });
+
+  var json = $.parseJSON(data);
+  // Add markers for all record
+  for (var rec of json.record) {
+    uluru = {lat: rec.latitude, lng: rec.longitude};
+    var marker = new google.maps.Marker({
+       position: uluru,
+       map: map,
+       label: {
+          text: "" + rec.uv,
+          color: 'white',
+          fontSize: "14px"
+       },
+    });
+  }
+}
+
+// Executes once the google map api is loaded, and then sets up and calls the handler
+function initMap() {
+  getUVRecord();
 }
 
 // Handle authentication on page load
